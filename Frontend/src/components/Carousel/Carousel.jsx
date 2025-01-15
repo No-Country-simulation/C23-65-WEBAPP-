161b98bdd376c4edcb130f3d8e800ff0514d1b6a
Gabriel Antony <gmamanican@unsa.edu.pe>
2025-01-14 22:32:10 -0500

    add gallery prop to Carousel component for dynamic arrow color and styling

--- Frontend/src/components/Carousel/Carousel.jsx
+++ Frontend/src/components/Carousel/Carousel.jsx
@@ -2,29 +2,30 @@ import Slider from "react-slick";
 import { GrNext, GrPrevious } from "react-icons/gr";
 
 import PropTypes from "prop-types";
-export const Carousel = ({ elements, color }) => {
+export const Carousel = ({ elements, color, gallery }) => {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    nextArrow: <GrNext color="white" />,
-    prevArrow: <GrPrevious color="white" />,
+    nextArrow: <GrNext color={`${gallery ? "black" : "white"}`} />,
+    prevArrow: <GrPrevious color={`${gallery ? "black" : "white"}`} />,
   };
 
   return (
     <div
       className={`pb-10 w-4/5 flex justify-center ${
         color ? `bg-[#013220] rounded-2xl` : ""
-      }`}
+      }
+      ${gallery ? `border border-black rounded-2xl py-10 ` : ""}`}
     >
       <Slider {...settings} className="w-5/6 ">
         {(elements || []).map((element, index) => (
           <div key={index}>{element}</div>
         ))}
-        <div>
+        {/* <div>
           <h3>2</h3>
-        </div>
+        </div> */}
       </Slider>
     </div>
   );
@@ -33,4 +34,5 @@ export const Carousel = ({ elements, color }) => {
 Carousel.propTypes = {
   elements: PropTypes.array.isRequired,
   color: PropTypes.bool,
+  gallery: PropTypes.bool,
 };
