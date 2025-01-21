92b7d0b699147f8089553626c397047a850ce7d9
Gabriel Antony <gmamanican@unsa.edu.pe>
2025-01-21 12:24:10 -0500

    enhance GalleryItemModal to accept imageURL prop and integrate zoom controls

--- Frontend/src/components/GalleryItem/GalleryItemModal.jsx
+++ Frontend/src/components/GalleryItem/GalleryItemModal.jsx
@@ -1,8 +1,9 @@
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import Modal from "@mui/material/Modal";
 import PropTypes from "prop-types";
+import { ControlsZoom } from "../ControlsZoom/ControlsZoom";
 
-export const GalleryItemModal = ({ open, onClose }) => {
+export const GalleryItemModal = ({ open, onClose, imageURL }) => {
   return (
     <Modal
       open={open}
@@ -11,11 +12,13 @@ export const GalleryItemModal = ({ open, onClose }) => {
     >
       <div className="p-0 rounded-lg shadow-md w-[60vw] h-auto flex flex-col border-none">
         <TransformWrapper limitToBounds={false}>
+          <ControlsZoom />
           <TransformComponent>
             <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1200px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"
-              alt="noche"
-              className="w-[60vw] object-contain"
+              // src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1200px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"
+              src={imageURL}
+              alt="Image zoomed"
+              className="w-[60vw] object-contain max-h-screen"
             />
           </TransformComponent>
         </TransformWrapper>
@@ -26,4 +29,5 @@ export const GalleryItemModal = ({ open, onClose }) => {
 GalleryItemModal.propTypes = {
   open: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
+  imageURL: PropTypes.string.isRequired,
 };
